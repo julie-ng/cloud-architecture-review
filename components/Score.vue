05de2c4239edf8524c883c71e6b60476c94c5882
Julie Ng <me@julie.io>
2021-08-15 16:58:03 +0200

    fix: missing tag for ssr and client side dom match

--- components/Score.vue
+++ components/Score.vue
@@ -2,22 +2,24 @@
 	<div>
 		<h1>Score (debug)</h1>
 		<table>
-			<tr>
-				<td>Complexity</td>
-				<td>{{ score.complexity }}</td>
-			</tr>
-			<tr>
-				<td>Operations</td>
-				<td>{{ score.operations }}</td>
-			</tr>
-			<tr>
-				<td>Security</td>
-				<td>{{ score.security }}</td>
-			</tr>
-			<tr>
-				<td>Price</td>
-				<td>{{ score.price }}</td>
-			</tr>
+			<tbody>
+				<tr>
+					<td>Complexity</td>
+					<td>{{ score.complexity }}</td>
+				</tr>
+				<tr>
+					<td>Operations</td>
+					<td>{{ score.operations }}</td>
+				</tr>
+				<tr>
+					<td>Security</td>
+					<td>{{ score.security }}</td>
+				</tr>
+				<tr>
+					<td>Price</td>
+					<td>{{ score.price }}</td>
+				</tr>
+			</tbody>
 		</table>
 	</div>
 </template>
