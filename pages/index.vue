1baa9cda26dcd673313cc835e68ec7bd6cf3f2a8
Julie Ng <me@julie.io>
2021-09-13 10:03:46 +0200

    main-page: use design, replace hello-world

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
-	<div>
-		<h1>Hello World</h1>
-		<a href="/questionnaire">Go to Questions &rarr;</a>
+	<div class="app-container">
+		<app-header/>
+
+		<div class="wrapper">
+			<h1>Welcome to the App</h1>
+			<a href="/questionnaire">Go to Questions &rarr;</a>
+		</div>
 	</div>
 </template>
