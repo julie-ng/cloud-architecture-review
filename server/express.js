60f37250ff997603c9335e9038701c0f60c209b2
Julie Ng <me@julie.io>
2021-10-14 11:26:13 +0200

    feat(http-logging): use express, move nuxt to middleware

---
+++ server/express.js
@@ -0,0 +1,41 @@
+'use strict'
+// process.env.DEBUG = 'nuxt:*'
+
+const app = require('express')()
+const pino = require('pino-http')()
+const helmet = require('helmet')
+const { loadNuxt, build } = require('nuxt')
+const graceful = require('./shutdown')
+const healthcheck = require('./routes/health')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+const PORT = process.env.PORT || '3100'
+
+app.use(pino)
+app.use(helmet())
+app.use('/health', healthcheck)
+
+let server
+const start = async function () {
+  const nuxt = await loadNuxt(isDevelopment ? 'dev' : 'start')
+  app.use(nuxt.render)
+  if (isDevelopment) {
+    build(nuxt)
+  }
+
+  try {
+    server = app.listen(PORT)
+  } catch (err) {
+    pino.logger.error(err)
+    process.exit(1)
+  }
+}
+
+const onShutdown = async function (signal) {
+  pino.logger.warn(`Received signal to terminate: ${signal}`)
+  await server.close()
+  process.exit()
+}
+
+start()
+graceful(onShutdown, pino.logger)
---
+++ server/routes/health.js
@@ -0,0 +1,11 @@
+const healthcheck = require('standard-healthcheck')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', healthcheck({
+  version: process.env.npm_package_version,
+  description: process.env.npm_package_description,
+  includeEnv: ['NODE_ENV', 'HOST']
+}))
+
+module.exports = router
