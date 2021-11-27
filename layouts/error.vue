deda976282f5e50e1d8bdca6183834cc454fe078
Julie Ng <me@julie.io>
2021-11-27 16:59:35 +0100

    feat(error): add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Oops - page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <NuxtLink class="btn btn-primary" to="/">Go to Homepage &rarr;</NuxtLink>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'basic'
+  }
+</script>
