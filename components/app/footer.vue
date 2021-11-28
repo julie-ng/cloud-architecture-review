6dbd67d24c003a09a23818c0b89d116930ac47f9
Julie Ng <me@julie.io>
2021-11-28 19:34:57 +0100

    footer: remove sha since that's in /health now

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -2,20 +2,6 @@
 	<footer class="app-footer">
 		<div class="wrapper">
 			<p>&copy; <a href="https://www.youtube.com/c/JulieNgTech/about" target="_blank">Julie Ng</a> 2021. Designed and made with &hearts; in Munich.</p>
-			<p v-if=isLocal><span id="js-build-no">(Local Development)</span></p>
-			<a v-else :href=treeUrl id="js-build-no" class="commit">Version</a>
 		</div>
 	</footer>
 </template>
-
-<script>
-export default {
-	data () {
-		return {
-			isLocal: this.$config.commitSha === '(unset)',
-			commitSha: this.$config.commitSha,
-			treeUrl: `${this.$config.repoUrl}/tree/${this.$config.commitSha}`
-		}
-	}
-}
-</script>
