f324a59b36d3a1832e32403ad66d82d356fa8e3c
Julie Ng <me@julie.io>
2021-11-30 18:37:14 +0100

    chore: rename ~/assets/css to ~/scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
 	css: [
-		'~/assets/css/main.scss'
+		'~/scss/main.scss'
 	],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
