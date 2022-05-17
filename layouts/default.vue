5818f23163ca2c375ead161b159f3969a5eaf0dd
Julie Ng <me@julie.io>
2022-05-17 17:36:17 +0200

    scoreboard: make sticky

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,14 @@
 	<div>
 		<app-header/>
 		<main class="container is-widescreen">
-			<div class="columns section">
+			<div class="columns mt-2">
 				<div class="column is-one-fifth-desktop">
 					<app-navigation/>
 				</div>
 				<div class="column">
 					<Nuxt/>
 				</div>
-				<div class="column is-one-fifth-desktop">
+				<div class="column is-one-fifth-desktop is-relative">
 					<app-scoreboard/>
 				</div>
 			</div>
