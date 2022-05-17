58f6c83d67c4c480690b22f189763533d853b5c9
Julie Ng <me@julie.io>
2022-05-17 17:32:33 +0200

    footer: smaller text

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -1,7 +1,8 @@
 <template>
 	<footer class="footer">
-		<div class="content mt-6 has-text-centered has-text-grey-light is-size-6">
-			<p>Copyright 2020-2021 <a class="has-text-grey-light" href="https://www.youtube.com/c/JulieNgTech/about" target="_blank">Julie Ng</a>. Designed and made with &hearts; in Munich.</p>
+		<div class="content mt-6 has-text-centered has-text-grey-light is-size-7">
+			<p>Copyright 2020-2021 <a class="has-text-grey" href="https://www.youtube.com/c/JulieNgTech/about" target="_blank">Julie Ng</a>
+			<br>Designed and made with &hearts; in Munich</p>
 		</div>
 	</footer>
 </template>
