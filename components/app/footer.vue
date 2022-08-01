225a59933acd3623dee2414b1d864207b32f5e9f
Julie Ng <me@julie.io>
2022-08-01 15:49:34 +0200

    footer: update year

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="app-footer footer">
 		<div class="content has-text-centered has-text-grey-light is-size-7">
-			<p>Copyright 2020-2021 <a class="has-text-grey" href="https://www.youtube.com/c/JulieNgTech/about" target="_blank">Julie Ng</a>
+			<p>Copyright 2021-2022 <a class="has-text-grey" href="https://www.youtube.com/c/JulieNgTech/about" target="_blank">Julie Ng</a>
 			<br>Designed and made with &hearts; in Munich</p>
 		</div>
 	</footer>
