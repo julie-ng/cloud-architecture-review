4f179b0bf797cf611f174af04475b6ae315615e3
Julie Ng <me@julie.io>
2022-08-27 10:49:51 +0200

    change website title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
-		title: 'Azure Kubernetes Architect',
+		title: 'Cloud In Real Life // Architecture',
 		htmlAttrs: {
 			lang: 'en',
 			class: 'has-navbar-fixed-top'
