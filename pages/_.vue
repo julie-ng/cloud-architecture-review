d348d31f0a5a87d8d9f77e5a7418df8d26eafef1
Julie Ng <me@julie.io>
2022-08-29 08:46:42 +0200

    style: line breaks

--- pages/_.vue
+++ pages/_.vue
@@ -11,7 +11,9 @@ export default {
 
   async asyncData ({ $content, app, params, error }) {
     const path = `/${params.pathMatch || 'index'}`
-    const [article] = await $content({ deep: true }).where({ path }).fetch()
+    const [article] = await $content({ deep: true })
+      .where({ path })
+      .fetch()
 
     if (!article) {
       return error({ statusCode: 404, message: 'Article not found' })
