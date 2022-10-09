0bc22e4c287f45751b35e1404fcd0b96a341b8d2
Julie Ng <me@julie.io>
2022-10-09 17:29:33 +0200

    footer: add reset button

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -1,5 +1,5 @@
 <template>
-	<footer class="app-footer footer">
+	<footer class="app-footer footer mt-3">
 		<div class="content has-text-centered has-text-grey-light is-size-7">
 			<p>Copyright 2021-2022 <a class="has-text-grey" href="https://www.youtube.com/c/JulieNgTech/about" target="_blank">Julie Ng</a>
 			<br>Designed and made with &hearts; in Munich
@@ -9,6 +9,18 @@
 				</a>
 			</p>
 
+			<button class="button is-small" @click="emptyStorage">Reset Session</button>
+
 		</div>
 	</footer>
 </template>
+
+<script>
+	export default {
+		methods: {
+			emptyStorage () {
+				this.$store.commit('decisions/reset')
+			}
+		}
+	}
+</script>
