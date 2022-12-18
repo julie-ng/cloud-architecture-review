8caaaddbdef553edfabebb0deb56daab705c1cff
Julie Ng <me@julie.io>
2022-12-18 17:19:30 +0100

    footer: add poc warning

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -1,5 +1,11 @@
 <template>
-	<footer class="app-footer footer mt-3">
+<div class="mt-3">
+	<article class="footer-poc-warning has-text-centered py-3">
+		<p>
+    	<strong>Warning</strong>: this is a proof of concept application used for DevOps demos. Please consider all content as placeholders.
+		</p>
+	</article>
+	<footer class="app-footer footer">
 		<div class="content has-text-centered has-text-grey-light is-size-7">
 			<p>Copyright 2021-2022 <a class="has-text-grey" href="https://www.youtube.com/c/JulieNgTech/about" target="_blank">Julie Ng</a>
 			<br>Designed and made with &hearts; in Munich
@@ -10,9 +16,9 @@
 			</p>
 
 			<button class="button is-small" @click="emptyStorage">Reset Session</button>
-
 		</div>
 	</footer>
+</div>
 </template>
 
 <script>
